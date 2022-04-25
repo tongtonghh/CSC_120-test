3c8bfbb48a87dfe7d64971bb7c88f06f0b65266c
黄彤 <huangtong@huangtongdembp.lan>
2022-04-24 20:51:41 -0400

    creat and print 3*3 board

---
+++ board.py
@@ -0,0 +1,10 @@
+def display_board():
+    print("Printing board:")
+    print(board[0])
+    print(board[1])
+    print(board[2])
+
+
+if __name__ == "__main__":
+    board = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
+    display_board()
